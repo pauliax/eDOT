4eec489b840bb2073e92d87a36c6a9d158fa9f56
Pauliax <pauliax6@gmail.com>
2021-02-06 14:42:38 +0200

    Fix faucet issue & upgrade dependencies

--- tasks/faucet.ts
+++ tasks/faucet.ts
@@ -1,21 +1,19 @@
 import * as fs from "fs";
 import { task } from "hardhat/config";
-// import { network} from "hardhat";
-// import hre from "hardhat";
 
 // This file is only here to make interacting with the Dapp easier,
 // feel free to ignore it if you don't need it.
 
 task("faucet", "Sends ETH and tokens to an address")
   .addPositionalParam("receiver", "The address that will receive them")
-  .setAction(async ({ receiver }) => {
-    // if (hre.network.name === "hardhat") {
-    //   console.warn(
-    //     "You are running the faucet task with Hardhat network, which" +
-    //       "gets automatically created and destroyed every time. Use the Hardhat" +
-    //       " option '--network localhost'"
-    //   );
-    // }
+  .setAction(async ({ receiver }, hre) => {
+    if (hre.network.name === "hardhat") {
+      console.warn(
+        "You are running the faucet task with Hardhat network, which" +
+          "gets automatically created and destroyed every time. Use the Hardhat" +
+          " option '--network localhost'",
+      );
+    }
 
     const addressesFile =
       __dirname + "/../frontend/src/contracts/contract-address.json";
@@ -25,25 +23,25 @@ task("faucet", "Sends ETH and tokens to an address")
       return;
     }
 
-    // const addressJson = fs.readFileSync(addressesFile);
-    // const address = JSON.parse(addressJson.toString());
+    const addressJson = fs.readFileSync(addressesFile);
+    const address = JSON.parse(addressJson.toString());
 
-    // if ((await hre.ethers.provider.getCode(address.Token)) === "0x") {
-    //   console.error("You need to deploy your contract first");
-    //   return;
-    // }
-    //
-    // const token = await ethers.getContractAt("Token", address.Token);
-    // const [sender] = await ethers.getSigners();
-    //
-    // const tx = await token.transfer(receiver, 100);
-    // await tx.wait();
-    //
-    // const tx2 = await sender.sendTransaction({
-    //   to: receiver,
-    //   value: ethers.constants.WeiPerEther,
-    // });
-    // await tx2.wait();
+    if ((await hre.ethers.provider.getCode(address.Token)) === "0x") {
+      console.error("You need to deploy your contract first");
+      return;
+    }
+
+    const token = await hre.ethers.getContractAt("Token", address.Token);
+    const [sender] = await hre.ethers.getSigners();
+
+    const tx = await token.transfer(receiver, 100);
+    await tx.wait();
+
+    const tx2 = await sender.sendTransaction({
+      to: receiver,
+      value: hre.ethers.constants.WeiPerEther,
+    });
+    await tx2.wait();
 
     console.log(`Transferred 1 ETH and 100 tokens to ${receiver}`);
   });
