059dc6e38f67dc6bae7e0153a6a358b02a01b9bd
jonasvag <jonasvag777@gmail.com>
2021-02-14 18:06:12 +0200

    Toggle dark/light theme

--- frontend/src/components/FAQButton.tsx
+++ frontend/src/components/FAQButton.tsx
@@ -2,7 +2,7 @@ import { HashLink } from "react-router-hash-link";
 
 export function FAQButton() {
   return (
-    <HashLink smooth to="/#faq" className="dark-text mx-4 p-0">
+    <HashLink smooth to="/#faq" className="theme-text mx-4 p-0">
       FAQ
     </HashLink>
   );
--- frontend/src/components/ThemeButton.tsx
+++ frontend/src/components/ThemeButton.tsx
@@ -1,11 +1,45 @@
+import { useContext, useEffect } from "react";
+import { ThemeContext } from "../contexts/Context";
+
 export function ThemeButton() {
+  const themeKey = "Theme";
+  const darkThemeClass = "dark-theme";
+  const dark = "dark";
+  const light = "light";
+
+  const { isDarkTheme, setIsDarkTheme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    if (!localStorage || !setIsDarkTheme) return;
+    const theme = localStorage.getItem(themeKey);
+    if (theme === dark) setIsDarkTheme(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps,
+  }, []);
+
+  useEffect(() => {
+    if (!localStorage || !document?.body) return;
+
+    if (isDarkTheme) {
+      localStorage.setItem(themeKey, dark);
+      document.body.classList.add(darkThemeClass);
+    } else {
+      localStorage.setItem(themeKey, light);
+      document.body.classList.remove(darkThemeClass);
+    }
+  }, [isDarkTheme]);
+
+  const toggleTheme = () => {
+    if (setIsDarkTheme) setIsDarkTheme(!isDarkTheme);
+  };
+
   return (
     <button
       type="button"
-      className="nes-btn is-error scroll-btn active"
+      className="nes-btn is-error scroll-btn active pt-0"
       title="Toggle dark/light theme"
+      onClick={toggleTheme}
     >
-      <span>*</span>
+      {isDarkTheme ? <span>☀️</span> : <span>🌘</span>}
     </button>
   );
 }
