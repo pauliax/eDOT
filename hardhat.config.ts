abfe9239efd404c77cde2cb12981a11b288b17a0
Pauliax <pauliax6@gmail.com>
2021-02-21 20:57:55 +0200

    Deploy to BSC testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import { HardhatUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-typechain";
@@ -8,6 +9,20 @@ import "@openzeppelin/hardhat-upgrades";
 // // testing the frontend.
 import "./tasks/faucet";
 
+const MNEMONIC = process.env.MNEMONIC;
+const INFURA_KEY = process.env.INFURA_KEY;
+
+const needsInfura = process.env.npm_config_argv &&
+  (process.env.npm_config_argv.includes('rinkeby') ||
+    process.env.npm_config_argv.includes('ropsten') ||
+    process.env.npm_config_argv.includes('live') ||
+    process.env.npm_config_argv.includes('bsctest'));
+
+if ((!MNEMONIC || !INFURA_KEY) && needsInfura) {
+  console.error('Please set a mnemonic and infura key.');
+  process.exit(0);
+}
+
 const config: HardhatUserConfig = {
   solidity: "0.7.6",
   // defaultNetwork: "hardhat",
@@ -16,6 +31,15 @@ const config: HardhatUserConfig = {
   //     chainId: 1337,
   //   },
   // },
+  networks: {
+    bsctest: {
+      url: `https://data-seed-prebsc-1-s2.binance.org:8545/`,
+      chainId: 0x61,
+      accounts: {
+        mnemonic: MNEMONIC,
+      },
+    }
+  }
 };
 
 export default config;
